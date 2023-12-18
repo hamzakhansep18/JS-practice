fa1d9f58c5131e22a2db99bb81be32ebd5340c40
hamzakhansep18 <hamzakhansep18@gmial.com>
2023-12-18 12:50:57 +0500

    config typeof

---
+++ typeof.js
@@ -0,0 +1,10 @@
+let age=33;
+console.log(typeof(age));
+ let name= "Hamza";
+ console.log(typeof(name));
+
+ let ageInString=String(age);
+ console.log(typeof ageInString)
+ let nameInNumber=Number(name);
+ console.log(typeof nameInNumber)
+
