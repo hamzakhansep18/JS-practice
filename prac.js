01406557ef16b080e177224688c2db87aad92bf4
hamzakhansep18 <hamzakhansep18@gmial.com>
2023-12-18 11:50:26 +0500

    my first commit

---
+++ prac.js
@@ -0,0 +1,15 @@
+// to check number is even or odd
+let num=5;
+if(num%2==0){
+    console.log('number is even')
+}else{
+    console.log('number is odd')
+}
+
+// to check variable is number or not
+let vari='12';
+if(typeof(vari)=='number'){
+    console.log('type of variable is number')
+}else{
+    console.log('type of varibale is not a number')
+}
