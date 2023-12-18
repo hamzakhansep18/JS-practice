febc47f31275bb13535b7c72cfa1c44903afc027
hamzakhansep18 <hamzakhansep18@gmial.com>
2023-12-18 12:22:08 +0500

    third commit

--- prac.js
+++ prac.js
@@ -1,9 +1,18 @@
-// find the largest of two number using if else
-let num=12;
+// find the largest of three numbers using if else
+let num=10;
 let num1=11;
+let num2=10;
 
 if (num>num1){
+    if(num>num2){
     console.log(`${num} is greater than ${num1}`)
-}else{
-    console.log(`${num1} is greater than ${num}`)
+    }else{
+        console.log(' num 2 is largest of three numbers')
+    }
+}else if(num1>num){
+    if(num1>num2){
+        console.log(`${num1} is greater than ${num}`)
+    }else{
+        console.log('num2 is the largest of three numbers')
+    }
 }
