f1cb891d3956f5723875db991ca38ec2a3a8b15b
hamzakhansep18 <hamzakhansep18@gmial.com>
2023-12-19 12:49:23 +0500

    commit

--- loops.js
+++ loops.js
@@ -2,7 +2,15 @@
 for(let i=1; i<=5; i++){
     let row=" ";
     for(let j=1; j<=5-i+1; j++){
-        row += "*";
+        row += "* ";
     }
     console.log(row);
+}
+
+for(let r=1; r<=5; r++){
+    let ros=" ";
+    for(let s=1; s<=r; s++){
+        ros += 'i ';
+    }
+    console.log(ros);
 }
