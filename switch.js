492592e6bb78fea9028142c3f3585c960a060418
hamzakhansep18 <hamzakhansep18@gmial.com>
2023-12-19 15:50:25 +0500

    first

---
+++ switch.js
@@ -0,0 +1,15 @@
+let x=4;
+switch(x){
+    case 1:
+        console.log('hello')
+        break;
+    case 2:
+        console.log('hi')
+        break;
+    case 3:
+        console.log('welocme')
+        break;
+    case 4:
+        console.log('How are you')
+        break;
+}
