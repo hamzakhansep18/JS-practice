1e321805ebaf3cbc35e38f3d0ae2bc491d48445c
hamzakhansep18 <hamzakhansep18@gmial.com>
2023-12-19 15:43:36 +0500

    do while loop

--- loops.js
+++ loops.js
@@ -1,16 +1,9 @@
 "use strict";
-for(let i=1; i<=5; i++){
-    let row=" ";
-    for(let j=1; j<=5-i+1; j++){
-        row += "* ";
-    }
-    console.log(row);
+let i=1;
+do{
+    console.log('pakistan')
+    i++;
 }
+while(i<=10);
 
-for(let r=1; r<=5; r++){
-    let ros=" ";
-    for(let s=1; s<=r; s++){
-        ros += 'i ';
-    }
-    console.log(ros);
-}
+// breaking the loop
