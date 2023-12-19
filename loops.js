34883da49d44bc6e841cb18c3b6180546674cd69
hamzakhansep18 <hamzakhansep18@gmial.com>
2023-12-19 12:40:18 +0500

    pat

--- loops.js
+++ loops.js
@@ -1,18 +1,7 @@
 "use strict";
-// triangle by using for loop
-for(let i=1; i<=4; i++){
-    let row=" ";
-   for(let j=1; j<=i; j++){
-    row += "* ";
-
-   }
-   console.log(row);
-}
-
-// pattern 2
 for(let i=1; i<=5; i++){
     let row=" ";
-    for(let j=1; j<=5; j++){
+    for(let j=1; j<=5-i+1; j++){
         row += "*";
     }
     console.log(row);
