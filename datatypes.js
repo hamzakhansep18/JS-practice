a479d1b809379f80063d1bd4e0f1a07da4a863dd
hamzakhansep18 <hamzakhansep18@gmial.com>
2023-12-22 17:42:48 +0500

    math function

---
+++ datatypes.js
@@ -0,0 +1,19 @@
+// numbers
+let a={
+    num:12.1,
+    num1: 12.6,
+    num2: 12.4,
+    num3: 12.7,
+    num4: 12.34,
+}
+console.log(Math.floor(a.num));
+console.log(Math.ceil(a.num1));
+console.log(Math.round(a.num2));
+console.log(Math.trunc(a.num3));
+console.log(a.num4.toFixed(1));
+console.log(parseInt("100px"));
+console.log(parseFloat("3.1.2px"));
+console.log(Math.random());
+console.log(Math.max(1, 2, 3, 4));
+console.log(Math.min(1,2,3,4));
+console.log(Math.pow(9,2));
