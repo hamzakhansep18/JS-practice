3063c442fd12b2768224e802cf6a7e774b97df6a
hamzakhansep18 <hamzakhansep18@gmial.com>
2023-12-27 15:31:00 +0500

    date and time

---
+++ datentime.js
@@ -0,0 +1,13 @@
+let mydate= new Date();
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toISOString());
+console.log(mydate.toJSON());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toTimeString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toLocaleTimeString());
+console.log(mydate.toUTCString());
+
+console.log(typeof mydate)
